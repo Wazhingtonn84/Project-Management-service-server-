761006d76f078d536f085dea0e5896c7ccf0849b
EMMANUEL KIMUTAI <wazhingtonn84@gmail.com>
2026-01-31 11:59:10 +0300

    Clerk auth integration

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
 
 const app =  express();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,4 +17,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is serving on port ${PORT}`);
-});
+}); 
