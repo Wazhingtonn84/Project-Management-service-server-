28a7bb5a5dee442a48871f4bbf48c0cf1809f775
EMMANUEL KIMUTAI <wazhingtonn84@gmail.com>
2026-01-31 22:41:36 +0300

    Inngest integration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 import { clerkMiddleware } from '@clerk/express';
 
 const app =  express();
@@ -14,6 +16,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Server is live!');
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.listen(PORT, () => {
   console.log(`Server is serving on port ${PORT}`);
